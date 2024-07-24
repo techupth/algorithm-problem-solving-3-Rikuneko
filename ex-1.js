89b985510394a49227d006073459db9a91b9db0e
Wascharapong Phummon <rickyharvey.w@gmail.com>
2024-07-24 16:08:02 +0700

    feat: ex.1 & ex.2

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,16 @@
+// เรามี Array `customers` ซึ่งบรรจุรายชื่อสมาชิก ให้ใช้ Bubble Sort Algorithm เพื่อเรียงลำดับสมาชิกตามลำดับตัวอักษร 
+// เช่น ถ้า `customers = ["Somchai", "Arthit", "Suchada", "Napasorn", "Pimchanok"]` 
+// เมื่อ Algorithm นี้ทำงานจะแสดงผลลัพธ์ออกมาบนหน้าจอเป็น
+//  `[ "Arthit", "Napasorn", "Pimchanok", "Somchai", "Suchada"]`
+
+customers = ["Somchai", "Arthit", "Suchada", "Napasorn", "Pimchanok"]
+
 function sortCustomerName(customers) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  return customers.sort();
 }
 
+console.log(sortCustomerName(customers))
+// Bubble Sort Algorithm มี Big O เป็นเท่าไร เพราะอะไร
 // ตอบคำถามตรงนี้จ้า
+// จำไม่ได้ครับ
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,35 @@
+// เรามี Array students ซึ่งบรรจุข้อมูลนักเรียน ให้เขียน Algorithm เพื่อเรียงลำดับนักเรียนตามคะแนนสอบ
+// โดยเรียงจากคนที่สอบได้คะแนนสูงสุดไปยังคนที่สอบได้คะแนนต่ำสุด เช่น
+// [
+//   { id: "S001", name: "Anan", score: 85 },
+//   { id: "S002", name: "Boon", score: 92 },
+//   { id: "S003", name: "Chai", score: 78 },
+//   { id: "S004", name: "Dao", score: 95 },
+//   { id: "S005", name: "Ek", score: 88 },
+//   { id: "S006", name: "Fah", score: 80 }
+// ];
+// เมื่อ Algorithm นี้ทำงานจะเห็นผลลัพธ์ดังนี้
+// [
+//   { id: "S004", name: "Dao", score: 95 },
+//   { id: "S002", name: "Boon", score: 92 },
+//   { id: "S005", name: "Ek", score: 88 },
+//   { id: "S001", name: "Anan", score: 85 },
+//   { id: "S006", name: "Fah", score: 80 },
+//   { id: "S003", name: "Chai", score: 78 }
+// ]
+
+students = [
+  { id: "S001", name: "Anan", score: 85 },
+  { id: "S002", name: "Boon", score: 92 },
+  { id: "S003", name: "Chai", score: 78 },
+  { id: "S004", name: "Dao", score: 95 },
+  { id: "S005", name: "Ek", score: 88 },
+  { id: "S006", name: "Fah", score: 80 },
+];
+
 function sortStudentsByScore(students) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  return students[score].sort()
 }
+
+console.log(sortStudentsByScore(students))
